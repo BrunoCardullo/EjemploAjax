c9200703dc692f2f9b48ac6d28f3264befdc1b7e
BrunoCardullo <brunocardullo@hotmail.com>
2016-09-26 20:39:25 -0300

    FuncionAjax

--- js/funcionesAjax.js
+++ js/funcionesAjax.js
@@ -1,7 +1,7 @@
 
 function MostrarError()
 {
-	var funcionAjax=$.ajax({url:"nexoNoExiste.php",type:"post",data:{queHacer:"MostrarTexto"}});
+	/*var funcionAjax=$.ajax({url:"nexoNoExiste.php",type:"post",data:{queHacer:"MostrarTexto"}});
 	funcionAjax.done(function(retorno){
 		$("#principal").html(retorno);
 		$("#informe").html("Correcto!!!");
@@ -12,11 +12,28 @@ function MostrarError()
 	});
 	funcionAjax.always(function(retorno){
 		//alert("siempre "+retorno.statusText);
-	});
+	});*/
+
+$.ajax({url:"NoExiste.php"}).then(function(respuesta)
+{
+alert("Primero" + respuesta);
+Console.info("Primero", respuesta);
+$("#principal").html(error);
+$("#informe").html("Correcto!!!");
+}, function(error)
+{
+alert("Segundo" + error);
+Console.info("Segundo", error);
+$("#principal").html(":(");
+$("#informe").html(error.responseText);
+}); //devuelve 2 retornos o callback, da un error o correcto.
+//ajax permite conectarte con el servidor o con una API.
+
 }
+
 function MostrarSinParametros()
 {
-	var funcionAjax=$.ajax({url:"nexoTexto.php"});
+	/*var funcionAjax=$.ajax({url:"nexoTexto.php"});
 
 	funcionAjax.done(function(retorno){
 		$("#principal").html(retorno);
@@ -29,7 +46,16 @@ function MostrarSinParametros()
 	funcionAjax.always(function(retorno){
 		//alert("siempre "+retorno.statusText);
 
-	});
+	});*/
+	$.ajax({url:"nexoTexto.php"}).then(function(retorno)
+		{
+			$("#principal").html(retorno);
+		    $("#informe").html("Correcto!!!");
+		}, function(error)
+		{
+			$("#principal").html(":(");
+			$("#informe").html(error.responseText);
+		});
 }
 
 function Mostrar(queMostrar)
